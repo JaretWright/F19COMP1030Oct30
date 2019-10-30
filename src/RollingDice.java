56992947734442d2c931e464f239f649efbb2cae
Jaret Wright <hkkA223>
2019-10-30 14:31:49 -0400

    Added logic to count the results

--- src/RollingDice.java
+++ src/RollingDice.java
@@ -4,7 +4,35 @@ public class RollingDice {
 
     public static void main(String[] args)
     {
-        System.out.printf("The die rolled %d%n", rollDie());
+        //variables to store how many times a value was rolled
+        int one =0;
+        int two=0;
+        int three=0;
+        int four=0;
+        int five=0;
+        int six=0;
+
+        for(int i=1; i<=100; i++) {
+            int dieValue = rollDie();
+            System.out.printf("%3d. The die rolled %d%n", i, dieValue);
+
+            if (dieValue==1)
+                one++;
+            else if (dieValue==2)
+                two++;
+            else if (dieValue==3)
+                three++;
+            else if (dieValue==4)
+                four++;
+            else if (dieValue==5)
+                five++;
+            else
+                six++;
+        }
+
+        System.out.printf("1: %d times      2: %d times%n", one, two);
+        System.out.printf("3: %d times      4: %d times%n", three, four);
+        System.out.printf("5: %d times      6: %d times%n", five, six);
     }
 
     /**
